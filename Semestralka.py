8f6482e90a98f6b88bae6cd8d1e317ed47d61b05
zajicm13 <zajicm13@fit.cvut.cz>
2017-12-27 11:19:41 +0100

    Přepracování aplikace pomocí tlačítek.

--- Semestralka.py
+++ Semestralka.py
@@ -1,29 +1,46 @@
-import tkinter
+import tkinter as tk
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
 from PIL import ImageTk
 from PIL import Image
 import numpy as np
 
 
-img = Image.open( "kvetina.jpg" )
-try:
-    data = np.asarray( img, dtype='uint8' )
-except SystemError:
-    data = np.asarray( img.getdata(), dtype='uint8' )
-data.setflags(write=1)
 
-width = data.shape[1]
-height = data.shape[0]
-root = tkinter.Tk()
-root.title("Editor")
-root.geometry(str(width)+"x"+str(height + 50)+"+600+300")
-root.resizable(width=False, height = False)
-
-
-photo = Image.fromarray(data.astype('uint8'))
-imgTK = ImageTk.PhotoImage(photo)
-canvas = tkinter.Canvas(root, width = width, height = height)
-image = canvas.create_image((width,height),image=imgTK, anchor=tkinter.SE)
-canvas.pack()
-
+class Editor(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.start()
+    def start(self):
+        self.parent.title("Editor")
+        self.parent.geometry("+500+100")
+        self.pack()
+        self.img_frame = tk.Label(self, border = 25)
+        self.img_frame.pack(fill=BOTH, expand=1)
+        self.btn_frame = Frame(self.parent)
+        self.btn_frame.pack(fill=BOTH, expand=1)
+        self.button_close = Button(
+            self.btn_frame, text="ZAVŘÍT", fg="red", command=self.btn_frame.quit
+        )
+        self.button_close.pack(side=LEFT, padx=5, pady=5)
+        self.button_load = Button(self.btn_frame, text="Otevřít", fg="blue", command=self.OpenImage)
+        self.button_load.pack(side=RIGHT, padx=5, pady=5)
+    def OpenImage(self):
+        filepath = askopenfilename(filetypes=([("Image files", "*.jpg;*.png;*.ppm")]))
+        self.filename = filepath
+        self.LoadImage()
+    def LoadImage(self):
+        self.img = Image.open(self.filename)
+        self.data = np.asarray(self.img)
+        width, height = self.img.size
+        size = str(width) + "x" + str(height+100) + "+500+100"
+        self.parent.geometry(size)
+        photo = ImageTk.PhotoImage(self.img)
+        self.img_frame.configure(image=photo)
+        self.img_frame.image = photo  # keep a reference!
+root = tk.Tk()
+Editor(root)
 
 root.mainloop()
